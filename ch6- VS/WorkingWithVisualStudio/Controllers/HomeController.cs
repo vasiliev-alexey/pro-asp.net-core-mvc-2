7eaef0e4e1528c1f05449e72a5897a54f74d0fc8
Aleksey Vasilyev <vasiljev.alexey@otr.ru>
2019-09-30 12:23:42 +0300

    Глава 6 - работа в VS

---
+++ ch6- VS/WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WorkingWithVisualStudio.Models;
+
+namespace WorkingWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products.Where( _ => _?.Price < 50));
+         
+
+     
+    }
+}

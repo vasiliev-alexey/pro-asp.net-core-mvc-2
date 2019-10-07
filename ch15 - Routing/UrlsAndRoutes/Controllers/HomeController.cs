acfce9455897dd9df7e315ca6d44a9cfd7dac33c
Aleksey Vasilyev <vasiljev.alexey@otr.ru>
2019-10-07 12:48:51 +0300

    Закончена глава 15

---
+++ ch15 - Routing/UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+    using Microsoft.AspNetCore.Routing;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Result", new Result { Controller = nameof(HomeController), Action = nameof(Index) });
+        }
+
+        public ViewResult CustomVariable(string id)
+        {
+            Result r = new Result { Controller = nameof(this.GetType), Action = nameof(this.CustomVariable), };
+            //r.Data["Id"] = RouteData.Values["id"];
+            r.Data["Id"] = id ?? "<novalue>";
+            return View("Result", r);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ch15 - Routing/UrlsAndRoutes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UrlsAndRoutes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
